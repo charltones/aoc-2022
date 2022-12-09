1d6dd98863acc88ef2e68cbe969250433e008dea
EC2 Default User <rob@charltones.com>
2022-12-09 13:46:32 +0000

    day4

---
+++ 04/day4-2.py
@@ -0,0 +1,14 @@
+def overlaps(r1, r2):
+    if (r1[0] >= r2[0] and r1[0] <= r2[1]) or (r2[0] >= r1[0] and r2[0] <= r1[1]):
+        return True
+    else:
+        return False
+        
+with open('input.txt') as input:
+    tot = 0
+    for line in input:
+        line = line.strip()
+        rs = line.split(',')
+        if overlaps([int(x) for x in rs[0].split('-')], [int(x) for x in rs[1].split('-')]):
+            tot += 1        
+print(tot)
